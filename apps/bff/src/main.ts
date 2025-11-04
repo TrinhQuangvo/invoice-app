a8aad7a2570832274b6b799e42af8d24db34711b
Kwang.FS.Dev <>
2025-11-04 20:31:17 +0700

    feat/#swagger: setup swager finished!

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,24 @@ async function bootstrap() {
     })
   );
   const port = AppModule.PORT || 3400;
+
+  const config = new DocumentBuilder()
+    .setTitle('E-invoice-bff API')
+    .setDescription('The E-invoice-bff API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Default JWT Authorization',
+      type: 'http',
+      in: 'header',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+    })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
