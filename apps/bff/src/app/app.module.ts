a94241ab70916c48c5a747be736c0758c1dbbf43
Kwang.FS.Dev <>
2025-11-05 08:47:21 +0700

    feat/#middleware: done

--- apps/bff/src/app/app.module.ts
+++ apps/bff/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CONFIGURATION } from '../configurations';
-
+import { LoggerMiddleware } from '@common/middlewares';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -18,4 +18,8 @@ export class AppModule {
   static GLOBAL_PREFIX = CONFIGURATION.GLOBAL_PREFIX;
   static NODE_ENV = CONFIGURATION.NODE_ENV;
   static PORT = CONFIGURATION.APP_CONFIG.PORT;
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
 }
