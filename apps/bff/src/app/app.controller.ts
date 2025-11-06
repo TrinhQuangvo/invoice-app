45f219fd737fa53fbd338d50ee050dfd3e187737
Võ Trịnh Quang <voquang1406@gmail.com>
2025-11-06 12:04:10 +0700

    feat/#response_type: set up finished (#10)
    Co-authored-by: Kwang.FS.Dev <>

--- apps/bff/src/app/app.controller.ts
+++ apps/bff/src/app/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ResponseDTO } from '@common/interfaces';
 @Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getData() {
-    return this.appService.getData();
+    const data = this.appService.getData();
+    return new ResponseDTO({ data });
   }
 }
